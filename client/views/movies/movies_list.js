acab81a32e5dc2fd75a2dcccb2d7d8a7b7e170a1
Andreas Heiberg <git@andreas-heiberg.com>
2013-05-26 16:44:18 +0200

    reverted to old subscription model
    Signed-off-by: Andreas Heiberg <git@andreas-heiberg.com>

--- client/views/movies/movies_list.js
+++ client/views/movies/movies_list.js
@@ -1,35 +1,52 @@
-Template.seenMovies.movies = function() {
-	console.log('test');
-	var list = [];
-	Views.find({ userId: Meteor.userId(), seen: true }).forEach(function(view) {
-		list.push( Movies.findOne(view.movieId) );
-	});
-	return list;
-};
-
-Template.unseenMovies.movies = function() {
-	console.log('test');
-	var list = [];
-	Views.find({ userId: Meteor.userId(), seen: false }).forEach(function(view) {
-		list.push( Movies.findOne(view.movieId) );
-	});
-	return list;
-};
-
-Template.unmarkedMovies.movies = function() {
-	console.log('test');
-	var list = [];
-	Views.find({ userId: Meteor.userId() }).forEach(function(view) {
-		list.push( view.movieId );
-	});
-
-	return Movies.find({_id: {$nin: list}});
-};
+Template.seenMovies.helpers({
+	options: function() {
+		var seenMovies = [];
+		Views.find({ seen: true }).forEach(function(view) {
+			seenMovies.push( view.movieId );
+		});
+ 
+		return {
+			find: {_id: {$in: seenMovies}},
+			//sort: {popularity: -1},
+			handle: moviesHandle
+		}
+	}
+});
+ 
+Template.unseenMovies.helpers({
+	options: function() {
+		var unseenMovies = [];
+		Views.find({ seen: false }).forEach(function(view) {
+			unseenMovies.push( view.movieId );
+		});
+ 
+		return {
+			find: {_id: {$in: unseenMovies}},
+			//sort: {popularity: -1},
+			handle: moviesHandle
+		}
+	}
+});
+ 
+Template.unmarkedMovies.helpers({
+	options: function() {
+		var markedMovies = [];
+		Views.find().forEach(function(view) {
+			markedMovies.push( view.movieId );
+		});
+ 
+		return {
+			find: {_id: {$nin: markedMovies}},
+			//sort: {popularity: -1},
+			handle: moviesHandle
+		}
+	}
+});
 
 Template.moviesList.helpers({
 	movies: function() {
-		console.log('test');
-		return Movies.find({}, {limit: moviesHandle.limit});
+		var options = {sort: this.sort, limit: this.handle.limit()};
+		return Movies.find(this.find, options);
 	},
 	moviesReady: function() {
 		return this.handle.ready();
@@ -43,11 +60,6 @@ Template.moviesList.helpers({
 Template.moviesList.events({
 	'click .load-more': function(event, template) {
 		event.preventDefault();
-		var options = {sort: this.sort, limit: this.handle.limit()};
-		Movies.find(this.find, options).forEach(function(movie) {
-			Meteor.call('toggleViewStatus', movie._id);
-			Meteor.call('toggleViewStatus', movie._id);
-		});
 		this.handle.loadNextPage();
 	}
 });

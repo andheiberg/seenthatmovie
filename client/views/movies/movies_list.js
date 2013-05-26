290a9fb141bd041c803835abbbb482eedc3e2d22
Andreas Heiberg <git@andreas-heiberg.com>
2013-05-26 17:23:05 +0200

    Revert "changed unmarked, seen, unseen restriction to publications instead of subscriptions"
    This reverts commit 93406e9ce378f1bcc7fe310a1e587197f6be9617.
    
    Signed-off-by: Andreas Heiberg <git@andreas-heiberg.com>

--- client/views/movies/movies_list.js
+++ client/views/movies/movies_list.js
@@ -1,29 +1,44 @@
 Template.seenMovies.helpers({
 	options: function() {
+		var seenMovies = [];
+		Views.find({ seen: true }).forEach(function(view) {
+			seenMovies.push( view.movieId );
+		});
+ 
 		return {
-			//find: {_id: {$in: seenMovies}},
+			find: {_id: {$in: seenMovies}},
 			//sort: {popularity: -1},
-			handle: seenMoviesHandle
+			handle: moviesHandle
 		}
 	}
 });
  
 Template.unseenMovies.helpers({
 	options: function() {
+		var unseenMovies = [];
+		Views.find({ seen: false }).forEach(function(view) {
+			unseenMovies.push( view.movieId );
+		});
+ 
 		return {
-			//find: {_id: {$in: unseenMovies}},
+			find: {_id: {$in: unseenMovies}},
 			//sort: {popularity: -1},
-			handle: unseenMoviesHandle
+			handle: moviesHandle
 		}
 	}
 });
  
 Template.unmarkedMovies.helpers({
 	options: function() {
+		var markedMovies = [];
+		Views.find().forEach(function(view) {
+			markedMovies.push( view.movieId );
+		});
+ 
 		return {
-			//find: {_id: {$nin: markedMovies}},
+			find: {_id: {$nin: markedMovies}},
 			//sort: {popularity: -1},
-			handle: unmarkedMoviesHandle
+			handle: moviesHandle
 		}
 	}
 });
@@ -31,7 +46,7 @@ Template.unmarkedMovies.helpers({
 Template.moviesList.helpers({
 	movies: function() {
 		var options = {sort: this.sort, limit: this.handle.limit()};
-		return Movies.find({}, options);
+		return Movies.find(this.find, options);
 	},
 	moviesReady: function() {
 		return this.handle.ready();
